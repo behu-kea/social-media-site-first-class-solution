094060bb1bd64a169a2aa45f85b6823a20e75439
Benjamin Hughes <behu@kea.dk>
2021-02-24 12:01:55 +0100

    init commit

---
+++ src/main/java/com/example/demo/controllers/SocialMediaController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+
+@Controller
+public class SocialMediaController {
+    private ArrayList<Post> posts = new ArrayList<>();
+
+    @GetMapping("/submit")
+    public String renderEventForm() {
+        return "post-form.html";
+    }
+
+    @PostMapping("/create-new-post")
+    public String createNewEvent(@RequestParam("title") String title, @RequestParam("visibility") String visibility) {
+        // simulating new event creation
+        Post post = new Post(title, visibility);
+        posts.add(post);
+
+        return "redirect:/dashboard";
+    }
+
+    @GetMapping("/dashboard")
+    @ResponseBody
+    public ArrayList renderDashboard() {
+        System.out.println(posts.get(0).title+ posts.get(0).visibility);
+        ArrayList<Post> publicPosts = new ArrayList<>();
+
+        for (Post post : posts) {
+            if(post.visibility.equals("public")) {
+                publicPosts.add(post);
+            }
+        }
+
+        return publicPosts;
+    }
+
+}
---
+++ src/main/java/com/example/demo/models/Post.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+public class Post {
+    public String title;
+    public String visibility;
+
+    public Post(String title, String visibility) {
+        this.title = title;
+        this.visibility = visibility;
+    }
+}
